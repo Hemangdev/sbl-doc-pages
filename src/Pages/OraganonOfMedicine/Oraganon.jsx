2932f6e5197e2ad758f467384d543db160e6fa48
Hemangdev <asthanahemang@gmail.com>
2024-09-04 14:33:55 +0530

    one dummy left also removed

--- src/Pages/OraganonOfMedicine/Oraganon.jsx
+++ src/Pages/OraganonOfMedicine/Oraganon.jsx
@@ -13,12 +13,7 @@ const homeoUpdates = [
         img: 'https://images.unsplash.com/photo-1563213126-a4273aed2016?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mzl8fG1lZGljaW5lJTIwbmV3c3xlbnwwfHwwfHx8MA%3D%3D',
         pdf1: pdf1
     },
-    {
-        id: 2,
-        title: 'Vital Force and Potentization',
-        img: 'https://images.unsplash.com/photo-1593491205049-7f032d28cf5c?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTJ8fG1lZGljaW5lJTIwbmV3c3xlbnwwfHwwfHx8MA%3D%3D',
-        pdf1: pdf1
-    },
+    
     
 
 
